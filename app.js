771ba34a803eb58c6ac277e76499dc2a744da863
Josue Dos Santos Marques <josue.santos@verdesmares.com.br>
2020-03-11 11:07:46 -0300

    radipo  ==  rapido

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ if ("serviceWorker" in navigator) {
       })
       .then(function (reg) {
         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-        reg.pushManager.subscribe().then( function(pushSubscription) {
-            console.log(pushSubscription.subscriptionId);
-            console.log(pushSubscription.endpoint);
-            // The push subscription details needed by the application
-            // server are now available, and can be sent to it using,
-            // for example, an XMLHttpRequest.
-          }, function(error) {
-        // During development it often helps to log errors to the
-        // console. In a production environment it might make sense to
-        // also report information about errors back to the
-        // application server.
-        console.log(error);
-      }
-    );
       })
     .catch(function(err) {
       console.log('Service Worker registration failed: ', err);
