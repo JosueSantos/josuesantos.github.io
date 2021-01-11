f70ddc44e18ebd3a2ee518aba79f7deddad9c4ce
Josué <josue.santos@svm.com.br>
2021-01-10 23:10:26 -0300

    Escala Creater

---
+++ EscalaCreater/BancoDeDados.js
@@ -0,0 +1,19 @@
+function retornarBancoDados(){
+		return bd = [
+			{nome:"David Guilherme" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Debora Kauany" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Guilherme" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Joao Paulo" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Joao Vinicius" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Levi Barroso" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Livia Oliveira" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Lucas Felipe" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Lucas Luan" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Rayane" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Samara" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Andreia" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Erick" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Geovana" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+			{nome:"Yasmin" 			, na:"S", rp:"S", ps:"S", sv:"S", sj:"S", semana:"S", sabdom:"S", noite:"S", dia:"S"},
+		];
+	}
